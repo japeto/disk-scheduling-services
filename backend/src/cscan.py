96dabecb9bfac607240dee56e60915d273f95a4d
HelkinJimenezM <165401178+HelkinJimenezM@users.noreply.github.com>
2024-11-25 17:28:23 -0500

    Create cscan.py

---
+++ backend/src/cscan.py
@@ -0,0 +1,26 @@
+# cscan.py
+def cscan(arm_position, lrequests, total_tracks):
+    lrequests.sort()
+    right = [r for r in lrequests if r >= arm_position]
+    left = [r for r in lrequests if r < arm_position]
+    
+    # Traverse right first
+    distance = 0
+    current_pos = arm_position
+    for request in right:
+        distance += abs(request - current_pos)
+        current_pos = request
+    
+    # Then wrap around and go to the beginning
+    distance += abs(total_tracks - 1 - current_pos)
+    current_pos = 0
+    for request in left:
+        distance += abs(request - current_pos)
+        current_pos = request
+    
+    average = distance / len(lrequests)
+    return {
+        "sequence": [arm_position] + right + left,
+        "average": average,
+        "distance": distance
+    }
